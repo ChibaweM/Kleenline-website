72dc0e1820b861994f2b3ea5f379c8f8244aa6cd
ChibaweM <chibawemkwenya@gmail.com>
2023-11-03 12:46:49 +0200

    adding and editing headers

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      this ios the header
+    </div>
+  )
+}
+
+export default Header
--- src/pages/Advertising/Landing.jsx
+++ src/pages/Advertising/Landing.jsx
@@ -2,12 +2,12 @@ import { useState, useEffect } from "react";
 import reactLogo from "../../assets/react.svg";
 import "../../App.css";
 import { useNavigate } from "react-router-dom";
-
+import Header from "../../components/Header";
 
 export default () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
   );
 };
